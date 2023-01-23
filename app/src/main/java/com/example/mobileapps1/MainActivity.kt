ed504918c26b00b4c3d25d229a605c137928a908
Saravanabalagi Ramachandran <saravanabalagi@gmail.com>
2023-01-23 16:18:03 +0000

    Change string programmatically Extract string resource

--- app/src/main/java/com/example/mobileapps1/MainActivity.kt
+++ app/src/main/java/com/example/mobileapps1/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.mobileapps1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myTextView = findViewById<TextView>(R.id.myTextView)
+        myTextView.text = getString(R.string.some_text)
     }
 }
