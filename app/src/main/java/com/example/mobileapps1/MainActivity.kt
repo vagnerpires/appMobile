d9ca52803fefd9df120653a1198500957ffbf009
Saravanabalagi Ramachandran <saravanabalagi@gmail.com>
2023-01-30 16:25:35 +0000

    Create activity OnClick navigate to new activity

--- app/src/main/java/com/example/mobileapps1/MainActivity.kt
+++ app/src/main/java/com/example/mobileapps1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mobileapps1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -11,16 +12,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val welcomeText = findViewById<TextView>(R.id.welcomeText)
-        welcomeText.text = getString(R.string.hello_dorset)
+        val helloText = findViewById<TextView>(R.id.welcomeText)
+        helloText.text = getString(R.string.hello_dorset)
 
-        val welcomeNameText = findViewById<TextView>(R.id.welcomeNameText)
+        val welcomeText = findViewById<TextView>(R.id.welcomeNameText)
         val saveButton = findViewById<Button>(R.id.saveButton)
         val nameEditText = findViewById<EditText>(R.id.nameEditText)
         val nameText = nameEditText.text
         saveButton.setOnClickListener {
-            val welcomeNameTextString = "Welcome, $nameText!"
-            welcomeNameText.text = welcomeNameTextString
+            val welcomeTextString = "Welcome, $nameText!"
+            welcomeText.text = welcomeTextString
+        }
+
+        val nextPageButton = findViewById<Button>(R.id.nextPageButton)
+        nextPageButton.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
         }
 
     }
