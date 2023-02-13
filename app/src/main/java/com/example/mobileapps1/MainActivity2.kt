db5c553174e4e39a5eb7b0457ffe986ea4b7e733
Saravanabalagi Ramachandran <saravanabalagi@gmail.com>
2023-02-13 15:24:39 +0000

    Understand Activity stack Default behaviour when navigating to a new activity onBackPressed ending an activity

--- app/src/main/java/com/example/mobileapps1/MainActivity2.kt
+++ app/src/main/java/com/example/mobileapps1/MainActivity2.kt
@@ -1,5 +1,31 @@
 package com.example.mobileapps1
 
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity2 : AppCompatActivity(R.layout.activity_main2)
+class MainActivity2 : AppCompatActivity(R.layout.activity_main2) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val nextPageButton = findViewById<Button>(R.id.nextPageButton)
+        nextPageButton.setOnClickListener {
+
+//            this creates new activity even if it exists in the stack
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+
+//            do not use onDestroy to end an activity
+//            onDestroy()
+            finish()
+        }
+    }
+
+    // deprecated, will be removed in the future versions of Android SDK
+    @Deprecated("Only for learning purposes")
+    override fun onBackPressed() {
+        super.onBackPressed()
+        Log.e("m2", "Back button pressed")
+    }
+}
