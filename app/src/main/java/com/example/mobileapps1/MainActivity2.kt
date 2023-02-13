0746b8862fc4de35bfefbd9584787a77767d3c6b
Saravanabalagi Ramachandran <saravanabalagi@gmail.com>
2023-02-13 14:31:51 +0000

    Minimal code required for activity without lifecycle functions

--- app/src/main/java/com/example/mobileapps1/MainActivity2.kt
+++ app/src/main/java/com/example/mobileapps1/MainActivity2.kt
@@ -1,30 +1,5 @@
 package com.example.mobileapps1
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.util.Log
 
-class MainActivity2 : AppCompatActivity() {
-    private val _tag = "main_activity"
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
-        Log.d(_tag, "Activity created")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.w(_tag, "Activity has been paused")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.e(_tag, "This activity has been resumed")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.e(_tag, "Activity has been destroyed")
-    }
-}
+class MainActivity2 : AppCompatActivity(R.layout.activity_main2)
