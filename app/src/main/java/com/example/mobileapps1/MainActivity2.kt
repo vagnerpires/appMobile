d62096045c52cc5bd97f3b9bf5be18e3dccc0124
Saravanabalagi Ramachandran <saravanabalagi@gmail.com>
2023-02-13 14:08:58 +0000

    Activity Lifecycle

--- app/src/main/java/com/example/mobileapps1/MainActivity2.kt
+++ app/src/main/java/com/example/mobileapps1/MainActivity2.kt
@@ -2,10 +2,27 @@ package com.example.mobileapps1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        Log.d("main_activity", "Activity created")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.w("main_activity", "Activity has been paused")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("main_activity", "This activity has been resumed")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("main_activity", "Activity has been destroyed")
     }
 }
