f8ecbb70dd79f6736a020510d974fe1ab250feaa
Saravanabalagi Ramachandran <saravanabalagi@gmail.com>
2023-02-20 14:05:25 +0000

    Snackbar with Action Toast Minimize keyboard RootViews

--- app/src/main/java/com/example/mobileapps1/MainActivity.kt
+++ app/src/main/java/com/example/mobileapps1/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.mobileapps1
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val rootView = window.decorView.rootView
+        val rootView = findViewById<View>(R.id.root_layout)
 
         val helloText = findViewById<TextView>(R.id.welcomeText)
         helloText.text = getString(R.string.hello_dorset)
@@ -19,9 +27,18 @@ class MainActivity : AppCompatActivity() {
         val saveButton = findViewById<Button>(R.id.saveButton)
         val nameEditText = findViewById<EditText>(R.id.nameEditText)
         val nameText = nameEditText.text
+
+
         saveButton.setOnClickListener {
             val welcomeTextString = "Welcome, $nameText!"
             welcomeText.text = welcomeTextString
+            it.hideKeyboard()
+            Toast.makeText(this, "Save button clicked", Toast.LENGTH_LONG).show()
+            val sb = Snackbar.make(rootView, "Save button clicked", Snackbar.LENGTH_INDEFINITE)
+            sb.setAction("dismiss") {
+                sb.dismiss()
+            }
+            sb.show()
         }
 
         val nextPageButton = findViewById<Button>(R.id.nextPageButton)
@@ -29,6 +46,12 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
         }
-
     }
+
+    fun View.hideKeyboard() {
+        val inputManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(windowToken, 0)
+    }
+
+
 }
