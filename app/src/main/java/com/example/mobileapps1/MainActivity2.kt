2624aa6408fee244b532ebabc2ff6c6c0b197985
Saravanabalagi Ramachandran <saravanabalagi@gmail.com>
2023-02-20 16:26:19 +0000

    Add text view programmatically Add 10000 of them (inefficiently)

--- app/src/main/java/com/example/mobileapps1/MainActivity2.kt
+++ app/src/main/java/com/example/mobileapps1/MainActivity2.kt
@@ -3,26 +3,27 @@ package com.example.mobileapps1
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity(R.layout.activity_main2) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val textViewsContainer = findViewById<LinearLayout>(R.id.textViewsContainer)
+        for (i in 0..10000) {
+            val textView = TextView(this).apply {
+                val t = "Something $i"
+                text = t
+            }
+            textViewsContainer.addView(textView)
+        }
+
         val nextPageButton = findViewById<Button>(R.id.nextPageButton)
         nextPageButton.setOnClickListener {
-
-//            this creates new activity even if it exists in the stack
             val intent = Intent(this, MainActivity::class.java)
-//                 https://developer.android.com/guide/components/activities/tasks-and-back-stack
-//                .apply {
-//                  flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-//                 }
             startActivity(intent)
-
-//            do not use onDestroy to end an activity
-//            onDestroy()
-//            finish()
         }
     }
 }
