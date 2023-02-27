8371c94f097eb5cf66bcbbd8d4fb06c449ab0cc4
Saravanabalagi Ramachandran <saravanabalagi@gmail.com>
2023-02-27 15:27:13 +0000

    ListView instead of adding textViews inside a for loop

--- app/src/main/java/com/example/mobileapps1/MainActivity2.kt
+++ app/src/main/java/com/example/mobileapps1/MainActivity2.kt
@@ -2,22 +2,23 @@ package com.example.mobileapps1
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.LinearLayout
+import android.widget.ListView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity(R.layout.activity_main2) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val textViewsContainer = findViewById<LinearLayout>(R.id.textViewsContainer)
-        for (i in 0..10000) {
-            val textView = TextView(this).apply {
-                val t = "Something $i"
-                text = t
-            }
-            textViewsContainer.addView(textView)
+        val listView = findViewById<ListView>(R.id.listView)
+        val strArray = Array(10000) { "Something ${it * 10}" }
+        listView.adapter = ArrayAdapter(this, R.layout.list_view_template, strArray)
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val textView = view.findViewById<TextView>(R.id.text_view)
+            Toast.makeText(this, "$position $id ${textView.text}", Toast.LENGTH_LONG).show()
         }
 
         val nextPageButton = findViewById<Button>(R.id.nextPageButton)
